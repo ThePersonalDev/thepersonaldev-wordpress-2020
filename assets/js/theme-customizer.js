df031b661c4abb95f8865405849bf6973d1449c2
Oz Ramos <thepersonaldev@gmail.com>
2020-04-10 23:13:06 -0700

    #2 Removes main.js, live edits callout href

--- assets/js/theme-customizer.js
+++ assets/js/theme-customizer.js
@@ -1,7 +1,15 @@
 (($, wp) => {
+  /**
+   * Live edit the navbar callout
+   */
   wp.customize('tpd_navbar_callout_label', (val) => {
     val.bind(newVal => {
       $('.navbar-callout').text(newVal)
+    })
+  })
+  wp.customize('tpd_navbar_callout_link', (val) => {
+    val.bind(newVal => {
+      $('.navbar-callout').attr('href', newVal)
     })
   })
 })(window.jQuery, window.wp)
