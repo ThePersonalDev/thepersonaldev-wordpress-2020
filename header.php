bd89a8b2229e9c6aea593b508c0ff0ae72518a19
Oz Ramos <thepersonaldev@gmail.com>
2020-04-11 18:58:41 -0700

    #12 Responsive menu

--- header.php
+++ header.php
@@ -9,25 +9,33 @@
 <body <?php body_class() ?>>
   <?php wp_body_open() ?>
 
-  <header class="container flex">
-    <div class="site-title">
-      <?php tcp_the_title_or_logo() ?>
-    </div>
-    <div class="flex text-uppercase">
-      <?php if (has_nav_menu('main')): ?>
-        <div class="desktop-main-menu-wrap">
-          <?php wp_nav_menu(['theme_location' => 'main']) ?>
-        </div>
-      <?php endif ?>
-      
-      <?php tcp_the_navbar_callout() ?>
+  <header class="header-main container">
+    <section class="flex">
+      <div class="site-title">
+        <?php tcp_the_title_or_logo() ?>
+      </div>
+      <div class="flex text-uppercase">
+        <?php if (has_nav_menu('main')): ?>
+          <div class="desktop-main-menu-wrap">
+            <?php wp_nav_menu(['theme_location' => 'main']) ?>
+          </div>
+        <?php endif ?>
+        
+        <?php tcp_the_navbar_callout() ?>
+  
+        <?php if (has_nav_menu('main')): ?>
+          <a class="mobile-main-menu-hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        <?php endif ?>
+      </div>
+    </section>
 
-      <?php if (has_nav_menu('main')): ?>
-        <a class="mobile-main-menu-hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
-      <?php endif ?>
-    </div>
+    <?php if (has_nav_menu('main')): ?>
+      <div class="mobile-main-menu-wrap">
+        <?php wp_nav_menu(['theme_location' => 'main']) ?>
+      </div>
+    <?php endif ?>
   </header>
