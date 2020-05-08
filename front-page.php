2c3ad37b00a5355f89ecec581fb69a67b70d6ee3
Oz Ramos <thepersonaldev@gmail.com>
2020-05-07 20:47:28 -0700

    Narrower content on single pages

--- front-page.php
+++ front-page.php
@@ -4,7 +4,6 @@ get_header(); ?>
     <?php if (have_posts()):
       while (have_posts()) {
         the_post();
-
         the_content();
       }
     endif; ?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 get_header(); ?>
   <main class="container">
     <?php if (have_posts()):
-      while (have_posts()) : ?>
+      while (have_posts()) : the_post(); ?>
         <header class="post-header">
           <h1 class="post-title"><?= the_title() ?></h1>
           <?php if (has_excerpt()): ?>
@@ -10,10 +10,10 @@ get_header(); ?>
           <?php endif ?>
         </header>
       
-        <?php the_post();
-
-        the_content();
-      endwhile;
+        <div class="post-content">
+          <?php the_content(); ?>
+        </div>
+      <?php endwhile;
     endif; ?>
   </main>
 <?php get_footer();
